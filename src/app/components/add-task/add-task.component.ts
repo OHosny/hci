1332aee2d81190c2d592bf22269abeab9169c0b0
OHosny <omarhosny216@gmail.com>
2024-04-18 20:17:34 +0200

    ay7aga

--- src/app/components/add-task/add-task.component.ts
+++ src/app/components/add-task/add-task.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { TasksService } from '../../services/tasks.service';
 import { Route, Router } from '@angular/router';
+import { Title } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-add-task',
@@ -10,14 +11,17 @@ import { Route, Router } from '@angular/router';
   styleUrl: './add-task.component.css'
 })
 export class AddTaskComponent implements OnInit {
-  constructor(public tasksService:TasksService,private router:Router){}
+  constructor(public tasksService:TasksService,private router:Router,public title:Title){this.title.setTitle('Add Task')}
   ngOnInit(): void {
     
   }
- saveTask(title:any,description:any)
+ saveTask(title:any,description:any,date:any)
  {
-  this.tasksService.saveTask(title.value,description.value)
+  this.tasksService.saveTask(title.value,description.value,date.value,false)
   this.router.navigate(['/'])
-
+ }
+ cancelTask()
+ {
+  this.router.navigate(['/'])
  }
 }
--- src/app/services/tasks.service.ts
+++ src/app/services/tasks.service.ts
@@ -1,30 +1,44 @@
 import { Injectable } from '@angular/core';
 import { Task } from '../interfaces/task';
+import { json } from 'stream/consumers';
 @Injectable({
   providedIn: 'root'
 })
 export class TasksService {
   tasks:Task[] = [
-    {
-      title:"First Task",
-      description:"First task description",
-    },
-    {
-      title:"os Task",
-      description:"First os task description",
-    }
+    
   ]
-  constructor() { }
+  constructor() {
+    let storedTasks = localStorage.getItem('tasks')
+    if(storedTasks){
+      this.tasks=JSON.parse(storedTasks)
+    }
+    else
+    {
+      this.tasks=[]
+    }
+   }
 
   deleteTask(i:number){
     this.tasks.splice(i,1);
+    this.storeTasks()
   }
-  saveTask(title:any,description:any)
+  saveTask(title:any,description:any,date:any,isCompleted:any)
   {
    this.tasks.push({
      title:title,
-     description:description
+     description:description,
+     date:date,
+     isCompleted:isCompleted
      
    })
+   this.storeTasks()
+  }
+  updateTask(id :any , item:any){
+    this.tasks[id]=item;
+    this.storeTasks()
+  }
+  storeTasks(){
+    localStorage.setItem('tasks',JSON.stringify(this.tasks))
   }
 }
